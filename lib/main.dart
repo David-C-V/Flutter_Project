3e27540f6b264b95b8cd84bfc773fd791f4e5c02
David-C-V <144165797+David-C-V@users.noreply.github.com>
2025-11-02 18:24:53 -0600

    3.5. Home screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,78 +8,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Widgets Básicos en Flutter',
+    
+      title: 'Mi Súper App',
+      debugShowCheckedModeBanner: false, 
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Ejemplo de Widgets'),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'hello world',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.blueAccent,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 20),
+        body: Stack(
+        fit: StackFit.expand,
+          children: [
+            Image.network(
+              'https://i.pinimg.com/736x/39/c1/7c/39c17cda63ffcf5cabca53f32ff222b3.jpg',
+               fit: BoxFit.cover,
+            ),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.star, color: Colors.orange),
-                  SizedBox(width: 5),
-                  Text('Text, Row, Column, Stack y Container'),
-                  SizedBox(width: 5),
-                  Icon(Icons.star, color: Colors.orange),
-                ],
-              ),
-              SizedBox(height: 20),
-
-              Container(
-                width: 200,
-                height: 100,
+            Center(
+              child: Container(
+               padding: EdgeInsets.all(24),
                 decoration: BoxDecoration(
-                  color: Colors.blue[100],
-                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.black.withOpacity(0.6),
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                alignment: Alignment.center,
-                child: Text(
-                  'Utilización de widgets',
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-              SizedBox(height: 20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Mi Súper App',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white, 
+                      ),
+                    ),
+                    SizedBox(height: 16), 
 
-              // STACK
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    width: 120,
-                    height: 120,
-                    color: Colors.redAccent,
-                  ),
-                  Container(
-                    width: 80,
-                    height: 80,
-                    color: Colors.yellowAccent,
-                  ),
-                  Container(
-                    width: 40,
-                    height: 40,
-                    color: Colors.greenAccent,
-                  ),
-                ],
+                    Text(
+                      '¡Te damos la bienvenida!',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white, 
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
